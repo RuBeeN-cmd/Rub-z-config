fb3fd6f95445afefc83fdbd26a0a18bb7ea62191
Ruben Rollin <63564987+RuBeeN-cmd@users.noreply.github.com>
2022-08-12 00:29:46 +0200

    Add files via upload

---
+++ rubz-s-theme/themes/main.c
@@ -0,0 +1,22 @@
+// comment
+/*
+comment
+*/
+
+
+//comment
+
+
+int main(int argc, char const *argv[])
+{
+	int name;
+	char *str = "Hello world !";
+
+	name = 0;
+	while (argc && name < 10)
+	{
+		name++;
+	}
+	
+	return 0;
+}
